74853439a77f99e12c8f63ed932980b7c7ea6d9f
Bret Antonio <bretjoseph@gmail.com>
2024-06-14 18:57:31 +0200

    Update

--- main.c
+++ main.c
@@ -17,6 +17,18 @@
 
 #include <webkit/webkit.h>
 
+static void send_command(GtkEntry *entry, GtkEntryIconPosition position, gpointer data)
+{
+    GtkEntryBuffer *buffer = gtk_entry_get_buffer(entry);
+    WebKitWebView *webview = (WebKitWebView *)data;
+    const char *script = gtk_entry_buffer_get_text(buffer);
+    if (position == GTK_ENTRY_ICON_SECONDARY)
+    {
+        webkit_web_view_evaluate_javascript(webview, script, -1, NULL, NULL, NULL, NULL, NULL);
+    }
+    gtk_entry_buffer_delete_text(buffer, 0, -1);
+}
+
 static void setup_webview(WebKitWebView *webview, WebKitLoadEvent load_event, gpointer user_data)
 {
     const char *script = "window.addEventListener('wheel', (e) => {"
@@ -41,17 +53,27 @@ static void create_window(GtkApplication *app, gpointer storage)
     const gchar *uri = "https://cncapp04.alliances.commandandconquer.com/451/index.aspx?langPreferred=en_US";
     GtkWidget *window = gtk_application_window_new(app);
     GtkWidget *webview = webkit_web_view_new();
+    GtkWidget *headerbar = gtk_header_bar_new();
+    GtkWidget *console = gtk_entry_new();
+    gtk_header_bar_set_show_title_buttons((GtkHeaderBar *)headerbar, TRUE);
+    gtk_header_bar_pack_start((GtkHeaderBar *)headerbar, console);
+    gtk_widget_set_size_request(console, 400, -1);
+    gtk_entry_set_icon_from_icon_name((GtkEntry *)console, GTK_ENTRY_ICON_SECONDARY, "mail-replied-symbolic");
+    gtk_window_maximize((GtkWindow *)window);
     gtk_window_set_child((GtkWindow *)window, webview);
+    gtk_window_set_titlebar((GtkWindow *)window, headerbar);
     gtk_window_present((GtkWindow *)window);
     gtk_window_set_title((GtkWindow *)window, "Command & Conquer: Tiberium Alliances");
-    gtk_widget_set_size_request(window, 1024, 576);
-    gtk_window_set_default_size((GtkWindow *)window, 1024, 576);
+    gtk_widget_set_size_request(window, 1280, 720);
+    gtk_window_set_default_size((GtkWindow *)window, 1280, 720);
     // WebKitSettings *settings = webkit_web_view_get_settings((WebKitWebView *)webview);
     WebKitNetworkSession *session = webkit_web_view_get_network_session((WebKitWebView *)webview);
     WebKitCookieManager *cookiejar = webkit_network_session_get_cookie_manager(session);
-    webkit_cookie_manager_set_persistent_storage(cookiejar, (gchar*)storage, WEBKIT_COOKIE_PERSISTENT_STORAGE_SQLITE);
+    webkit_cookie_manager_set_persistent_storage(cookiejar, (gchar *)storage, WEBKIT_COOKIE_PERSISTENT_STORAGE_SQLITE);
+    WebKitSettings *settings = webkit_web_view_get_settings((WebKitWebView *)webview);
+    webkit_settings_set_enable_write_console_messages_to_stdout(settings, TRUE);
     webkit_web_view_load_uri((WebKitWebView *)webview, uri);
-    // g_signal_connect(webview, "load-changed", (GCallback)setup_webview, NULL);
+    g_signal_connect(console, "icon-press", (GCallback)send_command, webview);
     /*GtkGesture *controller = gtk_gesture_zoom_new();
     g_signal_connect(controller, "scale-changed", (GCallback)prevent_zoom, NULL);
     gtk_widget_add_controller(webview, (GtkEventController*)controller);*/
@@ -59,7 +81,7 @@ static void create_window(GtkApplication *app, gpointer storage)
 
 int main(int response, char **name)
 {
-    gchar *path = (char*) malloc(strlen(g_get_home_dir()) + 36);
+    gchar *path = (char *)malloc(strlen(g_get_home_dir()) + 36);
     sprintf(path, "%s/.local/share/cncta/storage.db", g_get_home_dir());
 
     printf("Starting! ... %s\n", path);
